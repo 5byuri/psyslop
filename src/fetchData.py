de6f6f339cc4799c51c1eb9ae96001cec16feda7
Lars <lschwan43@gmail.com>
2025-04-10 22:36:34 +0200

    iterate thru videos and store them

--- src/fetchData.py
+++ src/fetchData.py
@@ -10,23 +10,46 @@ def youtubeFetch():
         sb.activate_cdp_mode(main.url)
         sb.open(main.url)
         
-        if invalidTokenCheck("saved_cookies/token.txt") == False:
+        if not invalidTokenCheck("saved_cookies/token.txt"):
             exit()
-            
-        sb.load_cookies(name="saved_cookies/token.txt")
-        
-        
+        try:
+            sb.load_cookies(name="saved_cookies/token.txt")
+            with open(file="saved_cookies/token.txt", mode= "r") as file:
+                print(file.read())
+        except NameError:
+            print("did not work")
+            sb.sleep(3)
+            sb.load_cookies(name="saved_cookies/token.txt")
+
         sb.open(main.url)
-       
-        #videos = sb.find_elements("style-scope ytd-rich-grid-renderer") 
+        sb.sleep(3)
+
+
+        sb.get_element('#video-title')  # id="video-title"
+
+        for i in range(20):
+            sb.scroll_to_bottom()
+        video_elements = sb.find_elements('a#video-title-link')
+        video_links = []
+        for video in video_elements:
+            href = video.get_attribute("href")
+            video_links.append(href)
+            print(href)
+
+
         elem_list = sb.find_elements("div.contents")
-        video = sb.find_elements("ytp-title-link yt-uix-sessionlink")
-        
+        video = sb.find_elements("href")
+        video = sb.find_elements("video-title-link")
+        sb.sleep(100000000)
+
         print(video)
         print(elem_list)
-        # Find all elements with href containing "/watch?v="
-        watch_links = sb.driver.find_elements("//a[contains(@href, '/watch?v=')]")
+        sb.sleep(3)
 
+# yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded
+        
+        # Find all elements with href containing "  "
+        # watch_links = sb.driver.find_elements("//a[contains(@href, '/watch?v=')]")
 def startUp():
     youtubeFetch()
 
