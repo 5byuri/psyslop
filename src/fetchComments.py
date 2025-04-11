2be12c1a6045c9b18d5672a2337896131d7ba53c
Lars <lschwan43@gmail.com>
2025-04-11 21:17:00 +0200

    now its scrolls to actually get the comments, this took way too long

--- src/fetchComments.py
+++ src/fetchComments.py
@@ -6,45 +6,32 @@ array = fetchVideos.youtubeFetch()
 print(array)
 
 with SB(uc=True, test=True, locale_code="en") as sb:
+    sb.activate_cdp_mode("youtube.com")
+    sb.load_cookies(name="saved_cookies/token.txt")
     for i in range(10):
-        sb.sleep(3)
-
-        
         sb.activate_cdp_mode(array[i])
-        sb.sleep(3)
-        print("not loaded")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        print("loaded")
-        sb.sleep(3)
-        print("click reject")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.sleep(3)
-        print("should work")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
-        sb.load_cookies(name="saved_cookies/token.txt")
 
-        sb.sleep(3)
-
-        print("loaded cookie")
+        sb.sleep(6)
+        sb.slow_scroll_to_element('ytd-continuation-item-renderer')
+        print("scrolled.")
+        sb.sleep(6)
+        # sb.uc_click('span:contains("Reject all")', reconnect_time=4)
         # sb.scroll_to_bottom("div#icon-label")
-        sb.scroll_to("div#icon-label")
-        sb.click("div#icon-label")
-        sb.click("div#item-with-badge div")
+        # sb.scroll_to("#ghost-cards")
+        # sb.click("div#icon-label")
+        # sb.click("div#item-with-badge div")
+        # sb.sleep(30)
+        # print("timer start 6 seconds to scroll down")
+        # sb.click("div#icon-label")
+        # sb.click("div#item-with-badge div")
+
+
+        sb.sleep(5)
         #sb.slow_scroll_to("div#icon-label")
         # sb.slow_scroll_to_element("div#icon-label")
 
 
 
-        sb.sleep(10)
 
 
 
@@ -53,3 +40,5 @@ with SB(uc=True, test=True, locale_code="en") as sb:
 #         sb.load_cookies(name="saved_cookies/token.txt")
 #         sb.activate_cdp_mode(fetchVideos.youtubeFetch()[i])
 #         sb.sleep(10)
+
+
